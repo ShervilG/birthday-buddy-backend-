28920b3bb50dcf95d7ba639759cc5070ef5ba970
ShervilG <wintergpt45@gmail.com>
2023-06-18 19:51:41 +0530

    Added base http client and TestService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TestService } from './services/test.service';
+import { Observable, of as observableOf } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'birthday-buddy-frontend';
+  tempData$: Observable<string> = observableOf('');
+
+  constructor(private readonly testService: TestService) {}
+
+  ngOnInit(): void {
+   this.tempData$ = this.testService.pingBackendService();
+  }
 }
---
+++ src/app/constants/url.ts
@@ -0,0 +1,3 @@
+// Test API URLs
+export const TEST_API_BASE_URL = "/test";
+export const PING_API_URL = "/ping";
